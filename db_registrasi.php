3ff5e72422b30b49ae70b5f824b65108960e35ca
RadenSehan <127505910+RadenSehan@users.noreply.github.com>
2023-06-04 19:21:26 +0700

    Update db_registrasi.php

---
+++ db_registrasi.php
@@ -0,0 +1,24 @@
+<?php
+$db = new mysqli("localhost", "root", "", "db_konser");
+
+$nama = $_POST["nama_lengkap"];
+$alamat = $_POST["alamat"];
+$email = $_POST["email"];
+$konser = $_POST["konser"];
+$foto = $_FILES["foto"]["name"];
+$tempname = $_FILES["foto"]["tmp_name"];
+$folder = "images/" . $foto;
+move_uploaded_file($tempname, $folder);
+
+$db = new mysqli("localhost", "root", "", "db_konser");
+
+if (mysqli_connect_errno()) {
+        die("Connection error: " . mysqli_connect_errno());
+} else {
+        $sql = "INSERT INTO tb_form (nama_lengkap, alamat, email, konser, foto)
+                VALUES ('$nama', '$alamat', '$email', '$konser', '$foto')";
+
+        mysqli_query($db, $sql);
+        echo "<script> alert ('Berhasil memasukkan data')</script>";
+}
+?>
